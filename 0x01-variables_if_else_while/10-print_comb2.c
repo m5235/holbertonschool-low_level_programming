80009b36b80e8143165a4c15252a26fb93cef927
m5235 <m.ladhari@outlook.com>
2020-02-06 22:42:17 +0100

    cc

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -4,15 +4,13 @@
 *Return: 0
 */
 int main(void)
-{int d1 = 0;
-int d2 = 0;
-for (d1 = 0 ; d1 < 9 ; d1++)
-{
-for (d2 = d1 + 1; d2 < 10 ; d2++)
-{putchar(d1 + '0');
-putchar(d2 + '0');
-if (d1 != 8)
+{int n = 0;
+for (n = 0 ; n < 100 ; n++)
+{putchar((n / 10) + '0');
+putchar((n % 10) + '0');
+if (n != 99)
 {putchar(',');
-putchar(' '); }}}
+putchar(' '); }
+}
 putchar('\n');
 return (0); }
