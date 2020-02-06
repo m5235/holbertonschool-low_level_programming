133c4228b21f4ef15cfd39f2c0e5dab6dd4c715d
m5235 <m.ladhari@outlook.com>
2020-02-06 20:18:06 +0100

    variable

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - entry point
+*@void: takes no arguments
+*Return: int
+*
+*/
+int main(void)
+{
+int i;
+for (i = 0; i < 10; ++i)
+{
+printf("%d ", i);
+}
+return (0);
+}
