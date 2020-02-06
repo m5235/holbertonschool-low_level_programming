2fca76d2ce8ad1d746ad0c303561becbc958d5e0
m5235 <m.ladhari@outlook.com>
2020-02-06 21:52:05 +0100

    variable

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+ 
+#include <stdio.h>
+/**
+*main - entry point
+*@void: takes no arguments
+*Return: int
+*
+*/
+int main(void)
+{
+char i;
+
+i = 'z';
+while (i <= 'a')
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
