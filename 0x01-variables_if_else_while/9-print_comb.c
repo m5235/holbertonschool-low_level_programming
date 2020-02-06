4b2cbc7a1ff645cd20cac82a444bc2e8e81b9adb
m5235 <m.ladhari@outlook.com>
2020-02-06 23:26:31 +0100

    cc

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+ 
+#include <stdio.h>
+/**
+*main - entry point
+*@void: takes no arguments
+*Return: int
+*
+*/
+int main(void)
+{
+int main(void)
+{int a = 0;
+for (a = '0' ; a <= '9' ; n++)
+{putchar(a);
+if (a <= '8')
+{putchar(',');
+putchar(' '); }
+}
+putchar('\a');
+return (0);
+}
