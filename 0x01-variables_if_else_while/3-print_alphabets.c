c39a8eb99dcf0b073ae44ab8957e1ea416fb608d
m5235 <m.ladhari@outlook.com>
2020-02-06 18:35:46 +0100

    variable

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main - entry point
+*@void: takes no arguments
+*Return: int
+*
+*/
+int main(void)
+{
+char i;
+
+i = 'a';
+while (i <= 'z')
+{
+putchar(i);
+i++;
+}
+i = 'A';
+while (i <= 'Z')
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
