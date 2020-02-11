a5d5d9a3c127f6fc3d395927d894f6c207aaf925
m5235 <m.ladhari@outlook.com>
2020-02-11 13:41:55 +0100

    put

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,18 +1,21 @@
-#include "holerton.h"
+#include "holberton.h"
 /**
-**
-*main- function
+*print_sign - prints the sign of a number
+*@n: integer
 *
-*
-* Return: Always 0.
+* Return: 0
 */
-int print_sign(int n);
-int i;
+int print_sign(int n)
 {
 if (n > 0)
+{
+_putchar('+');
 return (1);
-if else(n = 0)
+}
+else if (n == 0)
+{_putchar('0');
 return (-1);
-else
+}
+_putchar('-');
 return (0);
 }
