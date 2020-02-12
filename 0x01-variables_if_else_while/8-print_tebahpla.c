df13b1e87e9e24db5809a0382a2d3d0c38a58e4d
m5235 <m.ladhari@outlook.com>
2020-02-12 09:07:01 +0100

    lol

--- 0x01-variables_if_else_while/8-print_tebahpla.c
+++ 0x01-variables_if_else_while/8-print_tebahpla.c
@@ -4,11 +4,17 @@
 *Return: 0
 */
 int main(void)
-{int n = 0;
-char ch1 = 'a';
-for (n = '0' ; n <= '9' ; n++)
-{putchar(n); }
-for (ch0 = 'a' ; ch0 <= 'f' ; ch0++)
-{putchar(ch0); }
-putchar('\n');
-return (0); }
+{
+  int n = 0;
+  char ch1 = 'a';
+  for (n = '0' ; n <= '9' ; n++)
+  {
+    putchar(n);
+  }
+  for (ch0 = 'a' ; ch0 <= 'f' ; ch0++)
+  {
+    putchar(ch0);
+  }
+  putchar('\n');
+  return (0);
+}
