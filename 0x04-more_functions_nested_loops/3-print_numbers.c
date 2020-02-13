d7e6b412400d9e2b643b7addb72f223094d63539
m5235 <m.ladhari@outlook.com>
2020-02-13 16:51:08 +0100

    number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*main - entry point
+*@void: takes no arguments
+*Return: int
+*
+*/
+int main(void)
+{
+int i;
+for (i = '0'; i <= '9'; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
