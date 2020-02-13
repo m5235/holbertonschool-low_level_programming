3b4686a187a6c17eed73faf61f67f8b3f0271fcb
m5235 <m.ladhari@outlook.com>
2020-02-13 18:00:36 +0100

    print

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*print_most_numbers - entry point
+*@void: takes no arguments
+*Return: int
+*
+*/
+void print_most_numbers(void)
+{
+int i;
+i = '0';
+while (i <= '9')
+{
+if ((i != '2') && (i != '4'))
+putchar(i);
+i++;
+}
+putchar('\n');
+}
