53f7a4544e96b13e0b42219c4367c3d4b646f2ef
m5235 <m.ladhari@outlook.com>
2020-02-13 18:19:58 +0100

    lol

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,12 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = '0'; i <= '9'; i++)
-	{
-		if (i != '2' && i != '4')
-			_putchar(i);
-	}
-
-	_putchar('\n');
+for (i = '0'; i <= '9'; i++)
+{
+if (i != '2' && i != '4')
+_putchar(i);
+}
+_putchar('\n');
 }
