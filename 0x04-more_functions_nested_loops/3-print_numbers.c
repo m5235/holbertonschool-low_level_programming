ffb16a72a5d613b57f9ca38002e55a8ad1c4198a
m5235 <m.ladhari@outlook.com>
2020-02-13 17:31:15 +0100

    number

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,15 @@
 #include "holberton.h"
-
+#include "stdio.h"
 /**
- * print_numbers - Prints digit
- */
+* print_numbers - Prints digit
+*@void: return 
+*/
 void print_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 0; i <= 9; i++)
-		_putchar(i);
+for (i = '0'; i <= '9'; i++)
+_putchar(i);
 
-	_putchar('\n');
+_putchar('\n');
 }
