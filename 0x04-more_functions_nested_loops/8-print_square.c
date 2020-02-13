76d09b12194b5c053a841e169878c592ec1c499a
m5235 <m.ladhari@outlook.com>
2020-02-13 22:18:06 +0100

    hello

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -4,20 +4,20 @@
 *print_square -print the number
 *
 *@size: return value
-*/Return: 0
+*/ 
 void print_square(int size)
 
 {
 int a, b;
-if (size < 1)
-{
+if (size <= 0)
+
 _putchar('\n');
-}
-for (a = 0; a <= size; a++)
+
+for (a = 0; a < size; a++)
 {
-for (b = 0; b <= size; b++)
-{
+for (b = 0; b < size; b++)
+
 _putchar('#');
-}
 _putchar('\n');
+}
 }
