74e4dd12f4f929124280b01199234f9d4ae5c4c4
m5235 <m.ladhari@outlook.com>
2020-02-14 14:12:22 +0100

    lol

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,14 +6,18 @@
 *
 */
 int main(void)
-{int a = 0;
-for (a = 0 ; a < 100 ; n++)
-{putchar((a / 10) + '0');
+{
+int a = 0;
+for (a = 0 ; a <= 99 ; a++)
+{
+putchar((a / 10) + '0');
 putchar((a % 10) + '0');
 if (a != 99)
-{putchar(',');
-putchar(' '); }
+{
+putchar(',');
+putchar(' ');
 }
-putchar('\a');
+}
+putchar('\n');
 return (0);
 }
