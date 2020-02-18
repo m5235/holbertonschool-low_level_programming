ed9eab41360e0006d8b6afd0fe5b75334fbc736e
m5235 <m.ladhari@outlook.com>
2020-02-18 16:29:06 +0100

    lol

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+*puts_half - puts
+*@str: chaine
+*Return: 0
+*
+*/
+void puts_half(char *str)
+{
+int i, j = 0;
+while (str[j])
+j++;
+if (j % 2 == 0)
+{
+i = j / 2;
+}
+else
+{
+i = (j - 1) / 2;
+}
+while (i <= j)
+{
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
