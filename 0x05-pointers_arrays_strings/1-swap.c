282a25f0d1d524cd7ea4fdcb46d1202727ca1b1a
m5235 <m.ladhari@outlook.com>
2020-02-18 09:47:10 +0100

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * 
- * 
- * 
- */
+*swap_int - swaps the values
+*@a: chaine
+*@b: chaine
+*Return: 0
+*/
 void swap_int(int *a, int *b)
 
 {
 int x;
 x = *a;
-*a = *b ;
+*a = *b;
 *b = x;
-
- 
-
 }
