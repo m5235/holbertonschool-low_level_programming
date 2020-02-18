fff054f0e3dcf09a251c0973a2804ee3286d13f7
m5235 <m.ladhari@outlook.com>
2020-02-18 18:16:39 +0100

    print

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+*print_array - print
+*
+*@a: array
+*@n: number
+*/
+void print_array(int *a, int n)
+{
+int i = 0;
+while (i < n)
+{
+if (i < n  - 1)
+printf("%d, ", a[i]);
+else
+printf("%d\n", a[i]);
+i++;
+}
+}
