c392bce9a337ba7d6a5b119623d96bad6b0f406f
m5235 <m.ladhari@outlook.com>
2020-02-18 13:48:58 +0100

    lol

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,30 +1,26 @@
 #include "holberton.h"
-
 /**
-* 
-* 
-* 
+*rev_string - reverse chaine
+*@s: chaine
+*Return: 0
 */
 
- void rev_string(char *s)
-
- { int i, length = _strlen(s);
-  char tmp;
- 
-for (i = 0 ;i < length / 2; i++)
+void rev_string(char *s)
 {
-tmp = s[i] ;
-s[i] = s[length -i - 1] ;
-s[length - i -1] = tmp ;
+int i, length = _strlen(s);
+char tmp;
+for (i = 0; i < length / 2; i++)
+{
+tmp = s[i];
+s[i] = s[length - i - 1];
+s [length - i - 1] = tmp;
 }
 }
-
 /**
  *_strlen - copie legnth
  *@s: pointer
  *Return: 0
  */
-
 int _strlen(char *s)
 {
 int i;
