c8fc2d58c0ba7adafe0f58761634b2be1d98e812
m5235 <m.ladhari@outlook.com>
2020-02-19 10:46:36 +0100

    lol

--- 0x05-pointers_arrays_strings/9-main.c
+++ 0x05-pointers_arrays_strings/9-main.c
@@ -2,17 +2,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
+* main - check the code for Holberton School students.
+*
+* Return: Always 0.
+*/
 int main(void)
 {
-    char s1[98];
-    char *p;
-
-    p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
-    printf("%s", s1);
-    printf("%s", p);
-    return (0);
+char s1[98];
+char *p;
+p = _strcpy(s1, "First, solve the problem. Then, write the code\n");
+printf("%s", s1);
+printf("%s", p);
+return (0);
 }
