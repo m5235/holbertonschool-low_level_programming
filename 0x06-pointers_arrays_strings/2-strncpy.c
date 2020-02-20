67fe356fcb08892923295ba780368e8ed1a7255b
m5235 <m.ladhari@outlook.com>
2020-02-20 14:37:12 +0100

    lol

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+*_strncpy - copie string
+*@dest: chaine
+*@src: chaine
+*@n: number
+*Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+
+{
+int i;
+for (i = 0; (dest[i] != '\0') && (i < n); i++)
+dest[i] = src[i];
+return (dest);
+}
