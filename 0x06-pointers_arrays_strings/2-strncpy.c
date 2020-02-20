3c232d6616a57daf1d83a424812624cbe6e429cc
m5235 <m.ladhari@outlook.com>
2020-02-20 15:23:01 +0100

    update

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -7,12 +7,11 @@
 *Return: dest
 */
 char *_strncpy(char *dest, char *src, int n)
-
 {
 int i;
-for (i = 0; (dest[i] != '\0') && (i < n); i++)
+for (i = 0; (i < n) && (src[i] != '\0'); i++)
 dest[i] = src[i];
-for (; src[i] != '\0'; i++)
-;
-return (dest);
+for ( ; i < n; i++)
+dest[i] = '\0';
+return(dest);
 }
