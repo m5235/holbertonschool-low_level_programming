c5d33f25acc0981c110e674be2a72a26e7e7b03a
m5235 <m.ladhari@outlook.com>
2020-02-20 13:49:22 +0100

    string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,16 +2,14 @@
 /**
  * 
  * 
- */
+ */ 
 char *_strncat(char *dest, char *src, int n)
 
 { 
-    int a, i;
- 
-
-    for (i = 0; i < n && dest[i] = '\0'; i++)
-    ;
-    for (a = 0; a < n && src[a] = '\0'; a++)
-    dest[i + 1] = src[a];
-    return (dest);
+ int i, a;
+for (i = 0; dest[i] != '\0'; i++)
+;
+for (a = 0; (src[a] != '\0') && (a < n); a++)
+dest[i + a] = src[a];
+return (dest);
 }
