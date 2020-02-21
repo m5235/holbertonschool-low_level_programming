01ae24f8665068c736dc00efb355928f027f7fe5
m5235 <m.ladhari@outlook.com>
2020-02-21 20:55:17 +0100

    lol

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -7,11 +7,12 @@
  */
 char *string_toupper(char *ch)
 {
-    int i;
-for (i = 0; ch[i]!='\0'; i++) {
-   if(ch[i] >= 'a' && ch[i] <= 'z') {
-      ch[i] = ch[i] - 32;
-   }
+int i, j;
+for (i = 0; i != '\0'; i++)
+{
+for (j = 0; j <= i; j++) 
+if (ch[i] >= 'a' && ch[i] <= 'z')
+ch[i] = ch[i] - 32;
 }
-
+return (ch);
 }
