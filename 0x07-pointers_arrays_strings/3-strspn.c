e5afb2d4db4ba68610ae4f70a46060fe5c1a9548
m5235 <m.ladhari@outlook.com>
2020-02-24 22:58:50 +0100

    lol

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,16 +11,13 @@ unsigned int _strspn(char *s, char *accept)
 {
 int i = 0;
 int j;
+unsigned int n = 0;
 while (s[i] != '\0')
 i++;
+for (j = 0; accept[j] != '\0' ; j++)
 {
-for (j = 0; accept[j]; j++)
-{
-if (s == accept[j])
-break;
-if  (!accept[j])
-break;
+if (s[i] == accept[j])
+n++;
 }
-}
-return (i);
+return (n);
 }
