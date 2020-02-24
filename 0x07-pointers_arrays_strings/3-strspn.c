ed654033445a10228de64ded0c4209e1df971491
m5235 <m.ladhari@outlook.com>
2020-02-24 22:20:51 +0100

    lo

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ *_strspn -gets the length of a prefix
+ *@s: char
+ *@accept: char
+ *Return: i
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+int i = 0;
+int j;
+while (s[i] !='\0')
+i++;
+{
+for (j = 0; accept[j]; j++)
+{
+if (*s == accept[j])
+break;
+if  (!accept[j])
+break;
+}
+}
+return (i);
+}
