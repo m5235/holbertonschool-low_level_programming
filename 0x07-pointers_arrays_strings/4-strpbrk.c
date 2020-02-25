14cc075fc9fb72589a471993bfddca87b73c6a1b
m5235 <m.ladhari@outlook.com>
2020-02-25 09:46:40 +0100

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ *_strpbrk - searches a string for any of a set of bytes
+ *@s: char
+ *@accept: char
+ *Return: n
+ */
+char *_strpbrk(char *s, char *accept)
+
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+;
+for (j = 0; accept[j] ; j++)
+{
+if (s[j] == accept[j])
+return (&(s[j]));
+}
+return (0);
+}
