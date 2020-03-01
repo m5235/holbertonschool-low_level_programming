ddc2a45edadbfa70cde01304ca0b6b891dfba68a
m5235 <m.ladhari@outlook.com>
2020-03-01 18:09:04 +0100

    lol

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main -prints the number
+ *@argc: number argument
+ *@argv: array
+ *Return: 0
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+printf("%d\n", argc - 1);
+
+return (0);
+
+}
