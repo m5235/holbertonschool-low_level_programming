02c5acf882f93b7c2e933807f08b6c1dce67d29d
m5235 <m.ladhari@outlook.com>
2020-03-03 21:29:56 +0100

    task

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,31 +3,33 @@
 #include <stdlib.h>
 
 /**
- * 
- * 
- * 
+ **str_concat -concatenates two strings
+ * @s1: char
+ * @s2: char
+ * Return: NULL
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned i = 0 , z, j = 0 ;
-	char *n;
-	if (!(s1 == NULL))
-	{
-		for (i = 0; s1[i] != '\0'; i++)
-		;
-	}
-	if (!(s2 == NULL))
-	{
-	for (j = 0; s2[j] != '\0'; j++)
-	;
-	}
-	n = malloc((i + j + 1) * sizeof(char));
-	if (n == NULL)
-	return (NULL);
-	for (z = 0; z < i + j; z++)
-	n[z] = s1[z];
-	for (z = 0; z < i + j; z++)
-	n[z + i] = s2[z];
-	n[z] = '\0';
-	return (n);
+unsigned int i = 0, j = 0;
+unsigned int z;
+char *n;
+if (!(s1 == NULL))
+{
+for (i = 0; s1[i] != '\0'; i++)
+;
+}
+if (!(s2 == NULL))
+{
+for (j = 0; s2[j] != '\0'; j++)
+;
+}
+n = malloc((i + j + 1) * sizeof(char));
+if (n == NULL)
+return (NULL);
+for (z = 0; z < i + j; z++)
+n[z] = s1[z];
+for (z = 0; z < i + j; z++)
+n[z + i] = s2[z];
+n[z] = '\0';
+return (n);
 }
