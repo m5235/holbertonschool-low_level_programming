5f0b3626f742970dba0125114d6f2ebf222e926b
m5235 <m.ladhari@outlook.com>
2020-03-03 23:03:32 +0100

    lol

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,23 +13,27 @@ char *str_concat(char *s1, char *s2)
 unsigned int i = 0, j = 0;
 unsigned int z, len;
 char *n;
-if (!(s1 == NULL))
-{
+if (s1 == NULL)
+(s1 = "");
+else
 for (i = 0; s1[i] != '\0'; i++)
 ;
-}
-if (!(s2 == NULL))
-{
+if (s2 == NULL)
+(s2 = "");
+else
 for (j = 0; s2[j] != '\0'; j++)
 ;
-}
+
 n = malloc((i + j + 1) * sizeof(char));
 if (n == NULL)
 return (NULL);
-for (z = 0; z < i + j; z++)
+for (z = 0; z < i - 1; z++)
 n[z] = s1[z];
 for (len = 0; len < i + j; len++)
-n[len + i] = s2[len];
+{
+n[z] = s2[len];
+z++;
+}
 n[z] = '\0';
 return (n);
 }
