4395166d6100ce0d629f4c228e658fdfaf516e20
m5235 <m.ladhari@outlook.com>
2020-03-11 00:00:21 +0100

    first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog.h -new type
+ */
 
 struct dog
 {
@@ -13,6 +16,6 @@ void print_dog(struct dog *d);
 typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-int  _putchar ( char c);
+int  _putchar (char c);
 
 #endif
