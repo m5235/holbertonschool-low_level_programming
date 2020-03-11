2895e2fc0a8a9b3ae231aec93f41393d62315867
m5235 <m.ladhari@outlook.com>
2020-03-11 22:23:01 +0100

    task 2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ *array_iterator - function given as a parameter on each element of an array
+ *@array: pointer of array
+ *@size: size of the array
+ *@action: pointer
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+	if ((array == 0) && (action == 0))
+	return;
+	for (i = 0; i < size; i++)
+	action(array[i]);
+}
