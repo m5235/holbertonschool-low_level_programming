8f96db14d3f084372e9b5298210059993e276951
m5235 <m.ladhari@outlook.com>
2020-03-11 10:42:56 +0100

    task1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ *init_dog -init struct
+ *@d: pointer
+ *@name: pointer of char
+ *@age: float
+ *@owner: pointer of char
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+if (d == 0)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+return;
+}
