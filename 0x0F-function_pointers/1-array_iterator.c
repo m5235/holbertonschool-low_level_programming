be38cc12230c12f1151c8b255c0e54994f03e0ef
m5235 <m.ladhari@outlook.com>
2020-03-11 22:29:28 +0100

    task 2

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -8,7 +8,7 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i;
-	if ((array == 0) && (action == 0))
+	if ((array == NULL) && (action == NULL))
 	return;
 	for (i = 0; i < size; i++)
 	action(array[i]);
