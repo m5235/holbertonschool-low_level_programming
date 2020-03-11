cbb4750a21182295bff9144b28177d281b08db31
m5235 <m.ladhari@outlook.com>
2020-03-11 21:42:47 +0100

    lol

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,9 @@
 #include "function_pointers.h"
+#include <stdio.h>
 /**
  *print_name - funtion print name
  *@name:  name of print
  *@f: pointer
- *return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
