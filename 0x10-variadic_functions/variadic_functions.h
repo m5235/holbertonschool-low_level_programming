39e6e27cb4adeb4ade0812cef5796ba932fae4bd
m5235 <m.ladhari@outlook.com>
2020-03-15 21:10:34 +0100

    task1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,22 @@
-#ifndef VARIABLE_H
-#define VARIABLE_H
+#ifndef VARIADIC_H
+#define VARIADIC_H
+#include <stdarg.h>
 
+/**
+ * struct _types - Struct types
+ *
+ * @s: The type
+ * @p: The function associated
+ */
 
+typedef struct _types
+{
+	char *s;
+	void (*p)(va_list);
+} t;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-
 #endif
