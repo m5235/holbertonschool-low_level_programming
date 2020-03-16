382f2ff679ee4db8286f94e61317ac299dd3b77d
m5235 <m.ladhari@outlook.com>
2020-03-16 10:51:19 +0100

    advanced

---
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+
+msj db 'Hello, Holberton ',
+msj_length equ 		$ - 	msj			
+
+section .text
+	global main		
+
+main:				
+	mov rax,1
+	mov rdi,1
+	mov rsi,msj	
+	mov rdx,msj_length
+	syscall			
+
+	mov rax,60
+	mov rdi,0
+	syscall	
