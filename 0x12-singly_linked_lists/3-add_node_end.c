87a6359588703d7d778b074564b1185b3d6f1d72
m5235 <m.ladhari@outlook.com>
2020-03-23 00:01:16 +0100

    lol

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *add_node_end -adds a new node at the end
+ *@head: header
+ *@str: string
+ *Return: ptr
+ */
+list_t *add_node_end(list_t **head, const char *str)
+
+{
+	list_t *ptr = *head;
+	list_t *m;
+	unsigned int i;
+m = malloc(sizeof(list_t));
+if (m == NULL)
+return (NULL);
+for (i = 0; str[i]; i++)
+ptr->str = strdup(str);
+ptr->len = i;
+ptr->next = NULL;
+if (ptr == NULL)
+{
+ptr = m;
+return (m);
+}
+while (ptr && ptr->next)
+ptr = ptr->next;
+ptr->next = m;
+return (m);
+}
