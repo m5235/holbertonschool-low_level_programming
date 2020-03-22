10c750f96e11468aba3d06176062dffdf7ab5959
m5235 <m.ladhari@outlook.com>
2020-03-22 18:04:19 +0100

    lol

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,9 +1,19 @@
-#include "list.h"
+#include "lists.h"
 #include <stdio.h>
 
 /**
- * 
- * 
- * 
+ *list_len - function that returns the number of elements
+ *@h: header
+ *Return: i
  */
 size_t list_len(const list_t *h)
+{
+	const list_t *ptr = h;
+	unsigned int i;
+
+	for (i = 0; ptr != NULL; i++)
+{
+	ptr = ptr->next;
+}
+	return (i);
+}
