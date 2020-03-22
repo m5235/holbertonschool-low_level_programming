2883e540764bf1b44d502480c09bd457958e6017
m5235 <m.ladhari@outlook.com>
2020-03-22 22:04:26 +0100

    lol

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -16,9 +16,8 @@ list_t *add_node(list_t **head, const char *str)
 ptr = malloc(sizeof(list_t));
 if (ptr == NULL)
 return (NULL);
-if (head == NULL)
-return (NULL);
-for (i = 0; str[i]; i++)
+
+
 ptr->str = strdup(str);
 ptr->len = i;
 ptr->next = *head;
