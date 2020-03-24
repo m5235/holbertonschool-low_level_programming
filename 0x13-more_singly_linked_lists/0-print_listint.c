f8bd97b91f07234fae085411cdb4cccf954fe2f0
m5235 <m.ladhari@outlook.com>
2020-03-24 12:54:23 +0100

    lol

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,15 +2,15 @@
 #include "stdio.h"
 
 /**
- * 
- * 
- * 
- * 
+ *print_listint -prints all the elements
+ *
+ *@h: header file
+ *Return: i
  */
 size_t print_listint(const listint_t *h)
 
 {
-	const listint_t *ptr = h;
+const listint_t *ptr = h;
 unsigned int i;
 for (i = 0; ptr; i++)
 printf("%d\n", ptr->n);
