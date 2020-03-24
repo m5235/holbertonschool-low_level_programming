f7f5dfa41e3bc682fe656e783fa82e081d3e92d1
m5235 <m.ladhari@outlook.com>
2020-03-24 19:14:55 +0100

    lol

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ *free_listint2 -function that frees
+ *@head: header file
+ */
+
+void free_listint2(listint_t **head)
+
+{
+	listint_t *ptr = NULL;
+
+	if (head)
+		return;
+
+while (*head != NULL)
+{
+	ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+	*head = NULL;
+}
