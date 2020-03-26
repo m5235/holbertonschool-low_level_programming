0b14547107545d67e0ac091716dd9dacc87e0f93
m5235 <m.ladhari@outlook.com>
2020-03-26 18:12:50 +0100

    lol

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,32 +2,27 @@
 #include <stdio.h>
 
 /**
- *binary_to_uint -converts a binary number  
- *@b: binary number 
- *Return: 0 
+ *binary_to_uint -converts a binary number
+ *@b: binary number
+ *Return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	
-	int n = 0;
-	unsigned len = 0;
-
+int n = 0;
+unsigned int len = 0;
 	if (b == NULL)
 	{
 		return (0);
 	}
-
- while (b[len])
+while (b[len])
 	{
-		if (b[len] != '0' && b[len] != '1')
-			return (0);
+	if (b[len] != '0' && b[len] != '1')
+	return (0);
 len++;
-		
-	}
-	
-{	
-		n = (n* 2) + (*b - '0');
-	}
+}
+{
+n = (n * 2) + (*b - '0');
+}
 
 	return (n);
 }
