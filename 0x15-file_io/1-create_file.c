620b32063b890de822c072539cc1ce8cabcae400
m5235 <m.ladhari@outlook.com>
2020-03-31 21:28:42 +0100

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,14 +18,14 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 if (fd == -1)
 return (-1);
 
 for (i = 0; text_content[i]; i++)
 
 writer = write(fd, text_content, i);
-if ((writer == -1) || (writer != i))
+if ((writer < 0) || (writer != i))
 {
 return (-1);
 }
