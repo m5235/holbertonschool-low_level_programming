94f50684c2d9b125cc8d2b5339a48f440c1c8fbd
m5235 <m.ladhari@outlook.com>
2020-03-31 21:00:17 +0100

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		return (1);
 
-fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
 if (fd == -1)
 return (-1);
 
