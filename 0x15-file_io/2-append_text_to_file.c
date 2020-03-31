bf1b3e9d1e07ea14f8fefb85fef4a38bae0ff002
m5235 <m.ladhari@outlook.com>
2020-03-31 22:06:14 +0100

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -21,15 +21,14 @@ int append_text_to_file(const char *filename, char *text_content)
 fd = open(filename, O_WRONLY | O_APPEND);
 if (fd == -1)
 return (-1);
-if (fd < 0)
-		return (-1);
+
 while (text_content[i])
 i++;
 writer = write(fd, text_content, i);
 if (writer < 0)
-
+{
 return (-1);
-
+}
 
 return (1);
 
