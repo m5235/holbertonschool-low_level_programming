ac94bfa7bf5be4d5c47003b0de12f12ad0398df3
m5235 <m.ladhari@outlook.com>
2020-03-31 20:30:43 +0100

    create

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+/**
+ *create_file - Create a function that creates a file
+ *@filename: filename
+ *@text_content: data of file name
+ *Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+
+{
+	int fd, i;
+	int writer;
+
+	if (filename == NULL)
+	return (-1);
+	if (text_content == NULL)
+		return (1);
+
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (fd == -1)
+return (0);
+for (i = 0; text_content[i]; i++)
+
+writer = write(fd, text_content, i);
+if (writer < 0)
+{
+	return (-1);
+}
+return (1);
+
+}
