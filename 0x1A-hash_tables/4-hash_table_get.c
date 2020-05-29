dd2fa1d00acf002078f8e1208ee8746d540fe12c
m5235 <m.ladhari@outlook.com>
2020-05-29 22:03:01 +0100

    lol

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: hash table
+ * @key: key
+ * Return: the value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index = 0;
+
+	if (!ht || !key)
+		return (NULL);
+
+	if (!ht->array || !*key)
+		return (NUL)
+		index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
+	if (temp != NULL)
+	{
+		while (temp)
+		{
+			if (strcmp(temp->key, key) == 0)
+				return (temp->value);
+			temp = temp->next;
+		}
+	}
+	return (NULL);
+}
